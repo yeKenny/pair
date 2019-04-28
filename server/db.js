cce3c126e3660c50317f217a7ab7fb2a10d8b4d3
Kenny Ye <kye6932@gmail.com>
2019-04-28 10:54:21 -0400

    Fix database and demonstrate jest is functional for server directory

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const pkg = require('../../package.json')
+const pkg = require('../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '_test' : '')
 
