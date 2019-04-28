3c0025fcd95edf93ac878079ceed2ff1d0f98f39
Kenny Ye <kye6932@gmail.com>
2019-04-28 17:42:12 -0400

    Add button functionality for Buddy Request

--- src/components/SingleUser.jsx
+++ src/components/SingleUser.jsx
@@ -6,9 +6,17 @@ import NotFound from './NotFound';
 
 
 class SingleUser extends React.Component {
+  state = {
+    requestSent: false
+  }
+
   componentDidMount() {
     const userId = Number(this.props.match.params.id);
     this.props.fetchUser(userId)
+  }
+
+  handleClick = event => {
+    this.setState({requestSent: true})
   }
 
   render() {
@@ -17,14 +25,14 @@ class SingleUser extends React.Component {
     return user ? (
       <section className="section has-background-primary">
         <div className="box">
-          <article class="media">
-            <figure class="media-left">
-              <p class="image is-64x64">
+          <article className="media">
+            <figure className="media-left">
+              <p className="image is-64x64">
                 <img src={user.image} alt="User" />
               </p>
             </figure>
-            <div class="media-content">
-              <div class="content has-text-centered">
+            <div className="media-content">
+              <div className="content has-text-centered">
                 <p>
                   <strong>{user.username}</strong>
                   <br />
@@ -32,8 +40,11 @@ class SingleUser extends React.Component {
                 </p>
               </div>
             </div>
-            <div class="media-right">
-              <button class="button is-success">Add</button>
+            <div className="media-right">
+              {!this.state.requestSent
+              ? <button className="button is-success" onClick={this.handleClick}>Add</button>
+              : <button className="button is-success" disabled>Added</button>
+              }
             </div>
           </article>
         </div>
